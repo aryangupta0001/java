8a144a11357c1071a95a1d1b97114008767ab7da
Aryan Gupta <aryanguptabareilly067@gmail.com>
2025-09-26 02:22:20 +0530

    ArrayList

---
+++ arrays_strings.java
@@ -0,0 +1,19 @@
+public class arrays_strings {
+    public static void main(String[] args) {
+
+        int m = 1;
+
+        switch (m) {
+            case 1:
+                System.out.println(1);
+                break;
+
+            case 2:
+                System.out.println(2);
+
+            default:
+                break;
+        }
+
+    }
+}
---
+++ collections/array_list.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+
+public class array_list {
+    public static void main(String[] args) {
+        ArrayList<Integer> l1 = new ArrayList<>(3);
+        l1.add(1);
+        l1.add(3);
+        l1.add(5);
+
+        System.out.println(l1);
+
+        l1.add(1, 12);
+
+        System.out.println(l1);
+
+        ArrayList<Integer> l2 = new ArrayList<>();
+        l2.add(101);
+        l2.add(103);
+
+        l1.addAll(1, l2);                                           // Adds an ArrrayList to another ArrayList
+        System.out.println(l1);
+
+        // l2.clear();
+        System.out.println(l2.contains(102));
+
+        System.out.println(l1.indexOf(101));                            // Returns the index of the first occurence of theelement
+
+        l1.add(4, 101);
+
+        System.out.println(l1.lastIndexOf(101));                        // Returns the index of the last occurence of the element
+
+        l1.set(1, 150);                                     // Replaces the element at the specified position with the provided element
+
+        System.out.println(l1);
+
+        l1.remove(2);                                               // remove method removes/deleted the element at the specified position
+        System.out.println(l1);
+
+        l1.remove(Integer.valueOf("1"));                                // if any Object is provided in the remove method, it removes that object from the ArrayList
+        System.out.println(l1);
+
+    }
+
+}
