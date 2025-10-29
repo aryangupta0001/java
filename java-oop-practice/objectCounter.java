79fd7368f2368689d55fccd586554d05c69beea4
Aryan Gupta <aryanguptabareilly067@gmail.com>
2025-10-30 01:53:58 +0530

    commit

--- java-oop-practice/objectCounter.java
+++ java-oop-practice/objectCounter.java
@@ -1,3 +1,26 @@
+class Test {
+    static int counter = 0;
+
+    Test() {
+        counter++;
+    }
+}
+
 public class objectCounter {
-    
+    public static void main(String[] args) {
+
+        Test[] obj = new Test[20];
+
+        for (int i = 0; i < 10; i++) {
+            Test objects = new Test();
+        }
+
+        // or
+
+        // for (int i = 0; i < 10; i++) {
+        //     obj[i] = new Test();
+        // }
+
+        System.out.println(Test.counter);
+    }
 }
