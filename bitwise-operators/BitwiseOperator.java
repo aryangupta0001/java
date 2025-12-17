759df2d5a26513c1617d845a31aaeccc5ca3050b
Aryan Gupta <aryanguptabareilly067@gmail.com>
2025-12-18 02:02:33 +0530

    Bitwise Operator Intro

---
+++ bitwise-operators/BitwiseOperator.java
@@ -0,0 +1,43 @@
+public class BitwiseOperator {
+    public static void main(String[] args) {
+        /*
+        Bitwise operators work on Individual Bits of a the numbers, i.e Bitwise operation is done on each digit of its Binary Representation.
+
+        1. Bitwise AND  - &
+            2 & 3 --->
+              1 0         
+            & 1 1
+           =  1 0   = 2     i.e 2 & 3 = 2
+
+           "Bitwise AND with 1  always returns the number itself"
+
+           1 & 3 --->
+
+              0 1
+            & 1 1
+            = 0 1  = 1
+
+
+        
+
+        2. Bitwise  XOR - ^
+
+            It gives true if both values are different, i.e...
+            1 ^ 0 = 1
+            0 ^ 1 = 1
+            1 ^ 1 = 0
+            0 ^ 0 = 0
+
+            We can also say that A ^ B = A when B = 0
+            And, A ^ B = !A when B = 1
+
+
+            ^ is very helpful in :-
+            Finding specific Bit of a no.
+            Checking if a no. is even / odd.
+
+        */
+
+        
+    }
+}
