71a72fbe9945616cd9498f3afe6d2521ec721287
Aryan Gupta <aryanguptabareilly067@gmail.com>
2025-12-27 00:46:51 +0530

    Alternate approach - modifying the original array, improved memory complexity

--- java-dsa-practice/sorting/MergeSortRecursion.java
+++ java-dsa-practice/sorting/MergeSortRecursion.java
@@ -7,49 +7,55 @@ And in every Recursive call, to sort & merge it iterates over the whole array th
 And hence - O(n log(n))
 */
 
-
-
 import java.util.Arrays;
 
 public class MergeSortRecursion {
     public static void main(String[] args) {
         int[] nums = { 1, 4, 2, 7, 8, 3, 6, 5, 9, 0, 1, 5, 3, -987, -52 };
 
+        mergeSort(nums, 0, nums.length - 1);
+
         System.out.println(Arrays.toString(nums));
 
-        System.out.println(Arrays.toString(mergeSort(nums)));
     }
 
-    static int[] mergeSort(int[] nums) {
-        if (nums.length == 1)
-            return nums;
+    static void mergeSort(int[] nums, int beg, int end) {
+        if (beg == end)
+            return;
 
-        int mid = nums.length / 2;
+        int mid = beg + (end - beg) / 2;
 
-        int[] left = mergeSort(Arrays.copyOfRange(nums, 0, mid));
-        int[] right = mergeSort(Arrays.copyOfRange(nums, mid, nums.length));
+        mergeSort(nums, beg, mid);
+        mergeSort(nums, mid + 1, end);
 
-        return merge(left, right);
+        merge(nums, beg, mid, end);
+
+        System.out.println("");
+
     }
 
-    static int[] merge(int[] left, int[] right) {
-        int i = 0, j = 0, k = 0;
+    static void merge(int[] nums, int beg, int mid, int end) {
 
-        int[] arr = new int[left.length + right.length];
+        int[] arr = new int[end - beg + 1];
+        
+        int i = beg, j = mid + 1, k = 0;
 
-        while (i < left.length && j < right.length)
-            if (left[i] <= right[j])
-                arr[k++] = left[i++];
+        while (i <= mid && j <= end)
+            if (nums[i] <= nums[j])
+                arr[k++] = nums[i++];
 
             else
-                arr[k++] = right[j++];
+                arr[k++] = nums[j++];
 
-        while (i < left.length)
-            arr[k++] = left[i++];
+        while (i <= mid)
+            arr[k++] = nums[i++];
 
-        while (j < right.length)
-            arr[k++] = right[j++];
+        while (j <= end)
+            arr[k++] = nums[j++];
 
-        return arr;
+        k = 0;
+
+        for (i = beg; i <= end; i++)
+            nums[i] = arr[k++];
     }
 }
