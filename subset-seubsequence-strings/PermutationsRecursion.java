238ee9eb93ff7e303114c908af6acdd50c50f3d0
Aryan Gupta <aryanguptabareilly067@gmail.com>
2026-01-03 02:55:59 +0530

    collected the elements in a list

--- subset-seubsequence-strings/PermutationsRecursion.java
+++ subset-seubsequence-strings/PermutationsRecursion.java
@@ -1,23 +1,28 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class PermutationsRecursion {
     public static void main(String[] args) {
         String str = "abc";
         String processed = "";
 
-        permutations(processed, str);
+        System.out.println(permutations(processed, str));
     }
 
-    static void permutations(String processed, String unprocessed) {
+    static List<String> permutations(String processed, String unprocessed) {
 
-        if (unprocessed.length() == 0) {
-            System.out.println(processed);
-            return;
-        }
+        if (unprocessed.length() == 0)
+            return new ArrayList<String>(Arrays.asList(processed));
+
+        List<String> ans = new ArrayList<>();
 
         for (int i = 0; i <= processed.length(); i++) {
             String newProcessed = processed.substring(0, i) + unprocessed.charAt(0) + processed.substring(i);
-            
-            permutations(newProcessed, unprocessed.substring(1));
 
+            ans.addAll(permutations(newProcessed, unprocessed.substring(1)));
         }
+
+        return ans;
     }
 }
