28cdd9b78237efac5433d804822abdffd4480487
Aryan Gupta <aryanguptabareilly067@gmail.com>
2026-01-08 03:14:10 +0530

    commit

--- java-dsa-practice/maze-problems/TotalPath.java
+++ java-dsa-practice/maze-problems/TotalPath.java
@@ -4,7 +4,7 @@ public class TotalPath {
     public static void main(String[] args) {
         int[] count = new int[1];
         int r = 3;
-        int c = 7;
+        int c = 3;
 
         int[][] dp = new int[r + 1][c + 1];
 
@@ -18,12 +18,10 @@ public class TotalPath {
         if (dp[r][c] > -1)
             return dp[r][c];
 
-        if (r == 1 || c == 1) {
-            count[0]++;
-            System.out.println(count[0]);
+        if (r == 1 || c == 1)
             return 1;
-        }
 
+        
         dp[r][c] = countPaths(r - 1, c, count, dp) + countPaths(r, c - 1, count, dp);
         return dp[r][c];
     }
