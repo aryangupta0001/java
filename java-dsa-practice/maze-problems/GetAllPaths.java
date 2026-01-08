4cf0433953a8bcd071fca931a79fe89d67d6c714
Aryan Gupta <aryanguptabareilly067@gmail.com>
2026-01-09 01:06:57 +0530

    return ArrrayList for path

--- java-dsa-practice/maze-problems/GetAllPaths.java
+++ java-dsa-practice/maze-problems/GetAllPaths.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class GetAllPaths {
     public static void main(String[] args) {
         int r = 3;
@@ -6,21 +10,36 @@ public class GetAllPaths {
         System.out.println(getAllPaths(r, c, ""));
     }
 
-    static String getAllPaths(int r, int c, String path) {
+    // static String getAllPaths(int r, int c, String path) {
 
-        if(r==1 && c==1)
-            return path + "\n"; 
+    // if(r==1 && c==1)
+    // return path + "\n";
+
+    // String pathD = "";
+    // String pathR = "";
+
+    // if(r>1)
+    // pathD = getAllPaths(r-1, c, path +"D");
+
+    // if(c>1)
+    // pathR = getAllPaths(r, c-1, path + "R");
+
+    // return pathD + pathR;
+    // }
+
+    static List<String> getAllPaths(int r, int c, String path) {
+        if(r==1 && c==1){
+            return new ArrayList<String>(Arrays.asList(path));
+        }
+
+        List<String> ans = new ArrayList<>();
         
-        String pathD = "";
-        String pathR = "";
-
-
         if(r>1)
-            pathD = getAllPaths(r-1, c, path +"D");
-        
-        if(c>1)
-            pathR = getAllPaths(r, c-1, path + "R");
+            ans.addAll(getAllPaths(r-1, c, path + " D"));
 
-        return pathD + pathR;
+        if(c>1)
+            ans.addAll(getAllPaths(r, c-1, path + " R"));
+
+        return ans;
     }
 }
