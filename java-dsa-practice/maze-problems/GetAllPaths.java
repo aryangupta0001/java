3d47870f79e4ce55735bc58d7ca213e15e56e1d3
Aryan Gupta <aryanguptabareilly067@gmail.com>
2026-01-10 02:10:47 +0530

    added all ddirections + actual path

--- java-dsa-practice/maze-problems/GetAllPaths.java
+++ java-dsa-practice/maze-problems/GetAllPaths.java
@@ -7,41 +7,95 @@ public class GetAllPaths {
         int r = 3;
         int c = 3;
 
-        System.out.println(getAllPaths(r, c, ""));
+        boolean[][] visited = new boolean[r][c];
+
+        int[][] path = new int[r][c];
+
+        List<String> paths = new ArrayList<>(getAllPaths(0, 0, "", 1, path, visited));
+
+        for (String i : paths)
+            if (i.length() > 0)
+                System.out.println(i);
     }
+
+    // Return String coontaining of all paths, seperated by \n --->
 
     // static String getAllPaths(int r, int c, String path) {
 
-    // if(r==1 && c==1)
+    // if (r == 1 && c == 1)
     // return path + "\n";
 
     // String pathD = "";
     // String pathR = "";
 
-    // if(r>1)
-    // pathD = getAllPaths(r-1, c, path +"D");
+    // if (r > 1)
+    // pathD = getAllPaths(r - 1, c, path + "D");
 
-    // if(c>1)
-    // pathR = getAllPaths(r, c-1, path + "R");
+    // if (c > 1)
+    // pathR = getAllPaths(r, c - 1, path + "R");
 
     // return pathD + pathR;
     // }
 
-    static List<String> getAllPaths(int r, int c, String path) {
-        if(r==1 && c==1){
-            return new ArrayList<String>(Arrays.asList(path));
+    // Return an ArrayList of all paths, Deigonal move included --->
+
+    // static List<String> getAllPaths(int r, int c, String path) {
+    // if (r == 1 && c == 1)
+    // return new ArrayList<String>(Arrays.asList(path));
+
+    // List<String> ans = new ArrayList<>();
+
+    // if (r > 1)
+    // ans.addAll(getAllPaths(r - 1, c, path + " D"));
+
+    // if (c > 1)
+    // ans.addAll(getAllPaths(r, c - 1, path + " R"));
+
+    // if (r > 1 && c > 1)
+    // ans.addAll(getAllPaths(r - 1, c - 1, path + " d"));
+
+    // return ans;
+    // }
+
+    // All Directions included --->
+
+    static List<String> getAllPaths(int r, int c, String direction, int count, int[][] path, boolean[][] visited) {
+        path[r][c] = count;
+        
+        if (r == visited.length - 1 && c == visited[0].length - 1){
+            
+            System.out.println("\n" + direction);
+            for(int [] i : path)
+                System.out.println(Arrays.toString(i));
+
+            return new ArrayList<String>(Arrays.asList(direction));
         }
 
         List<String> ans = new ArrayList<>();
-        
-        if(r>1)
-            ans.addAll(getAllPaths(r-1, c, path + " D"));
 
-        if(c>1)
-            ans.addAll(getAllPaths(r, c-1, path + " R"));
+        if (visited[r][c])
+            return new ArrayList<String>(Arrays.asList(""));
 
-        if(r>1 && c>1)
-            ans.addAll(getAllPaths(r-1, c-1, path + " d"));
+        visited[r][c] = true;
+
+
+        if (r < visited.length - 1)
+            ans.addAll(getAllPaths(r + 1, c, direction + "D ", count + 1, path, visited));
+
+        if (c < visited[0].length - 1)
+            ans.addAll(getAllPaths(r, c + 1, direction + "R ", count + 1, path, visited));
+
+        if (c < visited[0].length - 1 && r < visited.length - 1)
+            ans.addAll(getAllPaths(r + 1, c + 1, direction + "d ", count + 1, path, visited));
+
+        if (r > 1)
+            ans.addAll(getAllPaths(r - 1, c, direction + "U ", count + 1, path, visited));
+
+        if (c > 1)
+            ans.addAll(getAllPaths(r, c - 1, direction + "L ", count + 1, path, visited));
+
+        visited[r][c] = false;
+        path[r][c] = 0;
 
         return ans;
     }
